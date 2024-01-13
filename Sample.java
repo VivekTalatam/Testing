311b012c7cdc5977094e22eeabbf737da6ccc993
Vivek Talatam <66777464+VivekTalatam@users.noreply.github.com>
2024-01-13 15:54:52 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,10 @@
+ class Sample {
+
+    public static void main(String ags[]){
+        int a = 7;
+        int b = 4;
+        System.out.println(a+b);
+        System.out.println("Executed....");
+    }
+    
+}
